12a9072658229c8810f5fb0477c96c7efd055754
Emmanuel Ofosu Amoah <barristaamoah@gmail.com>
2025-09-21 23:45:09 +0000

    0. Weather Recommendation Program

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -2,7 +2,7 @@
 
 current_weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
-#conditional statement
+
 if current_weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
 elif current_weather == "rainy":
