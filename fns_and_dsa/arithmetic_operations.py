3edfe8f834a203a2e6d4f9d074ec12fec032311c
Emmanuel Ofosu Amoah <barristaamoah@gmail.com>
2025-09-28 19:55:54 +0000

    0. Arithmetic Operations Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -10,6 +10,7 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 return "Error: Division by zero!"
-            return num1 / num2
+            else:
+                return num1 / num2
         case _:
             raise ValueError("Invalid operation! Use 'add', 'subtract', 'multiply', or 'divide'.")
