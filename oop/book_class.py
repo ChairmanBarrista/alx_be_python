644c706978c33973033b9d6d771365f85c281c1b
Emmanuel Ofosu Amoah <barristaamoah@gmail.com>
2025-10-12 19:07:59 +0000

    0. Dive into Python Magic Methods

---
+++ oop/book_class.py
@@ -0,0 +1,26 @@
+class Book:
+    def __init__(self, title: str, author: str, year: int):
+        """
+        Initialize a Book instance with title, author, and publication year.
+        """
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        """
+        Destructor that prints a message when the Book instance is deleted.
+        """
+        print(f"Deleting {self.title}")
+
+    def __str__(self) -> str:
+        """
+        Return a user-friendly string representation of the Book instance.
+        """
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self) -> str:
+        """
+        Return an official string representation that can recreate the Book instance.
+        """
+        return f"Book('{self.title}', '{self.author}', {self.year})"
